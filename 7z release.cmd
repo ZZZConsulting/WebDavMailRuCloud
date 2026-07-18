34240a2a7693c8db3e2aad520a0f5069077995f5
ZZZConsulting <ZZZConsulting@internet.ru>
2026-07-18 17:05:44 +0300

    Мелкие правки

--- 7z release.cmd
+++ 7z release.cmd
@@ -1,21 +1,68 @@
 set ver=1.26.07.18
 set options=-tzip -mx9 -r -sse -x!*.pdb -x!*dev*
+set dest=d:\temp\release
 
-REM "C:\Program Files\7-Zip\7z.exe" a %options% "BrowserAuthenticator-%ver%-net7.0-windows.zip" "BrowserAuthenticator\bin\Release\BrowserAuthenticator\bin\Release\net7.0-windows\*"
-"C:\Program Files\7-Zip\7z.exe" a %options% "BrowserAuthenticator-%ver%-net8.0-windows.zip" "BrowserAuthenticator\bin\Release\net8.0-windows\*"
-"C:\Program Files\7-Zip\7z.exe" a %options% "BrowserAuthenticator-%ver%-net9.0-windows.zip" "BrowserAuthenticator\bin\Release\net9.0-windows\*"
-REM "C:\Program Files\7-Zip\7z.exe" a %options% "BrowserAuthenticator-%ver%-net10.0-windows.zip" "BrowserAuthenticator\bin\Release\net10.0-windows\*"
+REM pushd BrowserAuthenticator\bin\Release\net7.0-windows
+REM "C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\BrowserAuthenticator-%ver%-net7.0-windows.zip" "*"
+REM popd
 
-REM "C:\Program Files\7-Zip\7z.exe" a %options% "WebDAVCloudMailRu-%ver%-dotNetCore3.1.zip" "WDMRC.Console\bin\Release\WDMRC.Console\bin\Release\netcoreapp3.1\*"
-REM "C:\Program Files\7-Zip\7z.exe" a %options% "WebDAVCloudMailRu-%ver%-dotNet5.zip" "WDMRC.Console\bin\Release\WDMRC.Console\bin\Release\net5.0\*"
-REM "C:\Program Files\7-Zip\7z.exe" a %options% "WebDAVCloudMailRu-%ver%-dotNet6.zip" "WDMRC.Console\bin\Release\WDMRC.Console\bin\Release\net6.0\*"
-REM "C:\Program Files\7-Zip\7z.exe" a %options% "WebDAVCloudMailRu-%ver%-dotNet7.zip" "WDMRC.Console\bin\Release\WDMRC.Console\bin\Release\net7.0\*"
-REM "C:\Program Files\7-Zip\7z.exe" a %options% "WebDAVCloudMailRu-%ver%-dotNet7Win.zip" "WDMRC.Console\bin\Release\WDMRC.Console\bin\Release\net7.0-windows\*"
-"C:\Program Files\7-Zip\7z.exe" a %options% "WebDAVCloudMailRu-%ver%-dotNet8.zip" "WDMRC.Console\bin\Release\net8.0\*"
-"C:\Program Files\7-Zip\7z.exe" a %options% "WebDAVCloudMailRu-%ver%-dotNet8Win.zip" "WDMRC.Console\bin\Release\net8.0-windows\*"
-"C:\Program Files\7-Zip\7z.exe" a %options% "WebDAVCloudMailRu-%ver%-dotNet9.zip" "WDMRC.Console\bin\Release\net9.0\*"
-"C:\Program Files\7-Zip\7z.exe" a %options% "WebDAVCloudMailRu-%ver%-dotNet9Win.zip" "WDMRC.Console\bin\Release\net9.0-windows\*"
-REM "C:\Program Files\7-Zip\7z.exe" a %options% "WebDAVCloudMailRu-%ver%-dotNet10.zip" "WDMRC.Console\bin\Release\net10.0\*"
-REM "C:\Program Files\7-Zip\7z.exe" a %options% "WebDAVCloudMailRu-%ver%-dotNet10Win.zip" "WDMRC.Console\bin\Release\net10.0-windows\*"
+pushd BrowserAuthenticator\bin\Release\net8.0-windows
+"C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\BrowserAuthenticator-%ver%-net8.0-windows.zip" "*"
+popd
 
-"C:\Program Files\7-Zip\7z.exe" a %options% "WebDAVCloudMailRu-%ver%-dotNet48.zip" "WDMRC.Console\bin\Release\net48\*"
+pushd BrowserAuthenticator\bin\Release\net9.0-windows
+"C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\BrowserAuthenticator-%ver%-net9.0-windows.zip" "*"
+popd
+
+REM pushd BrowserAuthenticator\bin\Release\net10.0-windows
+REM "C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\BrowserAuthenticator-%ver%-net10.0-windows.zip" "*"
+REM popd
+
+REM pushd WDMRC.Console\bin\Release\netcoreapp3.1
+REM "C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\WebDAVCloudMailRu-%ver%-dotNetCore3.1.zip" "*"
+REM popd
+
+REM pushd WDMRC.Console\bin\Release\net5.0
+REM "C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\WebDAVCloudMailRu-%ver%-dotNet5.zip" "*"
+REM popd
+
+REM pushd WDMRC.Console\bin\Release\net6.0
+REM "C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\WebDAVCloudMailRu-%ver%-dotNet6.zip" "*"
+REM popd
+
+REM pushd WDMRC.Console\bin\Release\net7.0
+REM "C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\WebDAVCloudMailRu-%ver%-dotNet7.zip" "*"
+REM popd
+
+REM pushd WDMRC.Console\bin\Release\net7.0-windows
+REM "C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\WebDAVCloudMailRu-%ver%-dotNet7Win.zip" "*"
+REM popd
+
+pushd WDMRC.Console\bin\Release\net8.0
+"C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\WebDAVCloudMailRu-%ver%-dotNet8.zip" "*"
+popd
+
+pushd WDMRC.Console\bin\Release\net8.0-windows
+"C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\WebDAVCloudMailRu-%ver%-dotNet8Win.zip" "*"
+popd
+
+pushd WDMRC.Console\bin\Release\net9.0
+"C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\WebDAVCloudMailRu-%ver%-dotNet9.zip" "*"
+popd
+
+pushd WDMRC.Console\bin\Release\net9.0-windows
+"C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\WebDAVCloudMailRu-%ver%-dotNet9Win.zip" "*"
+popd
+
+REM pushd WDMRC.Console\bin\Release\net10.0
+REM "C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\WebDAVCloudMailRu-%ver%-dotNet10.zip" "*"
+REM popd
+
+REM pushd WDMRC.Console\bin\Release\net10.0-windows
+REM "C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\WebDAVCloudMailRu-%ver%-dotNet10Win.zip" "*"
+REM popd
+
+pushd WDMRC.Console\bin\Release\net48
+"C:\Program Files\7-Zip\7z.exe" a %options% "%dest%\WebDAVCloudMailRu-%ver%-dotNet48.zip" "*"
+popd
+
